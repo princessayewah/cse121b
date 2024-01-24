f84d0f08c15a60e43488ca94c953de92097b3335
eboisetale <princessayewah@gmail.com>
2024-01-24 05:18:34 -0800

    wk 2 completed

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -33,34 +33,27 @@ imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 // Declare an array variable to hold your favorite foods
-const favFoodsArray = ['Pizza', 'Ice Cream', 'Sushi', 'Chocolate'];
-
-// Modify the HTML element with the id of food to display your favorite foods array
-foodElement.innerHTML = favFoodsArray.join('<br>');
+const favoriteFoods = ['Pizza', 'Ice Cream', 'Sushi', 'Chocolate'];
+// Append the initial array values onto the displayed content of the HTML element with the id of food
+foodElement.innerHTML = `${favoriteFoods.join(', ')}<br>`;
 
 // Declare and instantiate a variable to hold another single favorite food item
 const newFavoriteFood = 'Burgers';
 
 // Add the value stored in this new variable to your favorite food array using the push() method
-favFoodsArray.push(newFavoriteFood);
+favoriteFoods.push(newFavoriteFood);
 
-// Append the new array values onto the displayed content of the HTML element with the id of food
-foodElement.innerHTML += `<br>${favFoodsArray.join('<br>')}`;
+// Update the display to include the entire modified favorite foods array horizontally with commas
+foodElement.innerHTML += `${favoriteFoods.join(', ')}<br>`;
 
 // Remove the first element in the favorite food array
-favFoodsArray.shift();
+favoriteFoods.shift();
 
-// Append the array output showing the modified array, using a line break
-foodElement.innerHTML += `<br>${favFoodsArray.join('<br>')}`;
+// Update the display to include the entire modified favorite foods array horizontally with commas
+foodElement.innerHTML += `${favoriteFoods.join(', ')}<br>`;
 
 // Remove the last element in the favorite food array
-favFoodsArray.pop();
+favoriteFoods.pop();
 
-// Append the array output with this final modified favorite foods array
-foodElement.innerHTML += `<br>${favFoodsArray.join('<br>')}`;
-
-
-
-
-
-
+// Update the display to include the entire modified favorite foods array horizontally with commas
+foodElement.innerHTML += `${favoriteFoods.join(', ')}<br>`;
