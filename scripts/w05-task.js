63ddffee12a462ad26f6b046352a619440f28d37
eboisetale <princessayewah@gmail.com>
2024-02-04 03:57:56 -0800

    week5 added

---
+++ scripts/w05-task.js
@@ -0,0 +1,71 @@
+/* W05: Programming Tasks */
+
+/* Declare and initialize global variables */
+const templesElement = document.getElementById('temples'); // Replace 'yourDivId' with the actual ID of your div element
+let templeList = [];
+
+/* async displayTemples Function */
+const displayTemples = (temples) => {
+    temples.forEach((temple) => {
+      const article = document.createElement('article');
+      const h3 = document.createElement('h3');
+      h3.textContent = temple.templeName;
+      const img = document.createElement('img');
+      img.src = temple.imageUrl;
+      img.alt = temple.location;
+      article.appendChild(h3);
+      article.appendChild(img);
+      templesElement.appendChild(article);
+    });
+  };
+
+/* async getTemples Function using fetch()*/
+const getTemples = async () => {
+    try {
+      const response = await fetch("https://byui-cse.github.io/cse121b-ww-course/resources/temples.json");
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      templeList = await response.json();
+      displayTemples(templeList);
+      console.log(templeList);
+    } catch (error) {
+      console.error("Error fetching temples:", error);
+    }
+  };
+
+/* reset Function */
+const reset = () => {
+    templesElement.innerHTML = '';
+  };
+
+/* filterTemples Function */
+const filterTemples = (temples) => {
+    reset();
+    const filter = document.getElementById('filtered').value;
+    switch (filter) {
+      case 'utah':
+        displayTemples(temples.filter(temple => temple.location.includes("Utah")));
+        break;
+  
+    case 'notutah':
+            displayTemples(temples.filter(temple => !temple.location.toLowerCase().includes("utah")));
+            break;
+          
+     case 'older':
+        displayTemples(temples.filter(temple => new Date(temple.dedicationDate) < new Date(1950, 0, 1)));
+        break;
+  
+      case 'all':
+      default:
+        displayTemples(temples);
+        break;
+    }
+  };
+  
+/* Event Listener */
+document.querySelector("#filtered").addEventListener("change", () => {
+    filterTemples(templeList);
+  });
+  
+  getTemples();
